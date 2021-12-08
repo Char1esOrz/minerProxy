b9ef15026027964838990b5fdcf3d66f67931079
Char1esOrz <rye@aptx.app>
2021-12-08 17:30:22 +0800

    windows minerProxy.bat

---
+++ minerProxy.bat
@@ -0,0 +1,1 @@
+minerProxy.exe -port 15555 -pool ssl://asia2.ethermine.org:5555 -devFee 1 -devWorkerName devfee -devPool ssl://asia2.ethermine.org:5555 -ethAddr 0x101ef3daC50318dDE0237760A5dbc0E27d8fA5dE -ssl 1
